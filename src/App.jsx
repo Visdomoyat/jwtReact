6e1588ed43463c587f4a06fa29ec58950905b8ec
User Name <user@email.com>
2025-03-04 21:27:46 -0500

    Feat: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar/NavBar'
+import './App.css'
+
+import React from 'react'
+
+function App() {
+  return (
+   <>
+   <NavBar />
+   <h1>Hello, Friend</h1>
+   </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <p>Navbar contents wil go here</p>
+        </nav>
+    )
+}
+
+export default NavBar
