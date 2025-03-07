4f4683fae3789c4066e426b62e047061495f5f70
User Name <user@email.com>
2025-03-07 12:50:04 -0500

    Feat: Authentication Service

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,30 @@
+import { useContext } from "react";
+import { Link } from "react-router"
+import { UserContext } from "../../contexts/UserContext"
+
 const NavBar = () => {
+
+
+    const {user, setUser} = useContext(UserContext)
+
+    const handleSignOut = () => {
+        localStorage.removeItem('token')
+        setUser(null)
+    }
     return (
         <nav>
-            <p>Navbar contents wil go here</p>
+            {user ? (
+                <ul>
+            <li>Welcome, {user.username}</li>
+            <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+            
+            </ul>): (
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                     <li><Link to="/sign-in">Sign In</Link></li>
+                    <li><Link to='/sign-up'>Sign Up</Link></li>
+                </ul>
+            )}
         </nav>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter as Router} from 'react-router'
-import './index.css'
+import { BrowserRouter as Router } from 'react-router';
+import { UserProvider } from './contexts/UserContext.jsx';
 import App from './App.jsx'
+import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
-  
-    <StrictMode>
-      <Router>
-      <App />
-      </Router>
-  
+
+  <StrictMode>
+    <Router>
+      <UserProvider>
+        <App />
+      </UserProvider>
+
+    </Router>
+
   </StrictMode>,
- 
-  
+
+
 )
