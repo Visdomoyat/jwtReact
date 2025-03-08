5ffcdfcc33e9267203068f70ffcd19fb6d34382c
User Name <user@email.com>
2025-03-07 21:39:16 -0500

    Feat: Navbar/Landing page

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Landing() {
+  return (
+    <main>
+        <h1>Hello, you are on the landing page for visitors</h1>
+        <p>Sign up now, or sign in to see your supepr secret dashboard!</p>
+    </main>
+  )
+}
+
+export default Landing
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,32 +1,46 @@
-import { useContext } from "react";
-import { Link } from "react-router"
-import { UserContext } from "../../contexts/UserContext"
+import { Link } from 'react-router';
+import { useContext } from 'react';
+import { UserContext } from '../../contexts/UserContext';
 
 const NavBar = () => {
+  const { user, setUser } = useContext(UserContext);
 
+  const handleSignOut = () => {
+    // Clear the token from localStorage
+    localStorage.removeItem('token');
+    // Clear the user state
+    setUser(null);
+  };
 
-    const {user, setUser} = useContext(UserContext)
+  return (
+    <nav>
+      {user ? (
+        <ul>
+          <li>Welcome, {user.username}</li>
+          <li>
+            <Link to='/'>Dashboard</Link>
+          </li>
+          <li>
+            <Link to='/' onClick={handleSignOut}>
+              Sign Out
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/sign-in'>Sign In</Link>
+          </li>
+          <li>
+            <Link to='/sign-up'>Sign Up</Link>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+};
 
-    const handleSignOut = () => {
-        localStorage.removeItem('token')
-        setUser(null)
-    }
-    return (
-        <nav>
-            {user ? (
-                <ul>
-            <li>Welcome, {user.username}</li>
-            <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
-            
-            </ul>): (
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                     <li><Link to="/sign-in">Sign In</Link></li>
-                    <li><Link to='/sign-up'>Sign Up</Link></li>
-                </ul>
-            )}
-        </nav>
-    )
-}
-
-export default NavBar
+export default NavBar;
